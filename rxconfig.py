99ef618bd71d81df6c4e5891ce63deef134296aa
matiasmierezsuarez <matiassamu5@gmail.com>
2025-12-18 23:29:28 -0300

    Se corrigen los archivos que tuvieron conflictos

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,9 @@
 import reflex as rx
 
 config = rx.Config(
-<<<<<<< HEAD
+
     app_name="nuevo_intento",
-=======
-    app_name="dashboard",
->>>>>>> 83c717dee2a017a42a92b8aaa8df21bbab98a084
+
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
